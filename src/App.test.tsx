cbd6f0c42c9c8575f1e12b63c29a0b130d92a849
John Ruble <john.ruble@atomicobject.com>
2020-09-17 22:36:41 -0400

    use typescript feature not supported by babel (const enum)

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -5,5 +5,10 @@ import App from './App';
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
+
+  const enum TestEnum {
+    A = 1,
+    B = A * 2,
+  };
   expect(linkElement).toBeInTheDocument();
 });
